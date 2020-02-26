9194010b2753877ab98939acb88e1ed2b6eeff5c
Payton Mitchell <payton016@gmail.com>
2020-02-25 19:57:42 -0500

    interfaces

---
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/OwnerService.java
@@ -0,0 +1,18 @@
+package guru.springframework.petclinic.services;
+
+import java.util.Set;
+
+import guru.springframework.petclinic.model.Owner;
+
+public interface OwnerService {
+	
+	Owner findByLastName(String lastName);
+	
+	Owner findById(Long id);
+	
+	Owner save(Owner owner);
+	
+	Set<Owner> findAll();
+	
+
+}
