e3bfe2c9d638f6722a5835e8f4794f12d3bb5f87
Payton Mitchell <payton016@gmail.com>
2020-03-19 23:55:38 -0400

    adding spring config closes #22

--- pet-clinic/pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
@@ -16,12 +16,11 @@ public class DataLoader implements CommandLineRunner {
 	private final OwnerService ownerService;
 	private final VetService vetService;
 
-	
-	public DataLoader() {
-		// TODO Auto-generated constructor stub
-		ownerService = new OwnerServiceMap();
-		vetService = new VetServiceMap();
+	public DataLoader(OwnerService ownerService, VetService vetService) {
+		this.ownerService = ownerService;
+		this.vetService = vetService;
 	}
+
 
 	@Override
 	public void run(String... args) throws Exception {
