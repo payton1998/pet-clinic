0aac8f92093fbf66ddacdcc462eae16d8cfeffbb
Payton Mitchell <payton016@gmail.com>
2020-06-09 12:07:49 -0400

    Added Visit Entity closes #38

--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,8 +30,16 @@ public class Pet extends BaseEntity {
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
 	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 	
 	
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
+	}
 	public String getName() {
 		return name;
 	}
