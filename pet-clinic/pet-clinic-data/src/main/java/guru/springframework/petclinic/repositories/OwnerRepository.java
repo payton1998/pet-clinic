66b869a3ac06d8d9878a9aaf84ed09499642c6ca
Payton Mitchell <payton016@gmail.com>
2020-06-10 09:20:54 -0400

    added spring data jpa for owners. closes #40

--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/repositories/OwnerRepository.java
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/repositories/OwnerRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import guru.springframework.petclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long>{
+	
+	Owner findByLastName(String lastName);
 
 }
