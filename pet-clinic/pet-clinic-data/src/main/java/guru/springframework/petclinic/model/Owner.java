1c661e1fc3394b27ee63bfeb60c1080dde49f2bb
Payton Mitchell <payton016@gmail.com>
2020-06-12 10:20:35 -0400

    fix builder annotation

--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -17,12 +17,22 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
 	
+	
+	@Builder
+	public Owner(Long id, String firstName, String lastName, String address, String city, String telephone,
+			Set<Pet> pets) {
+		super(id, firstName, lastName);
+		this.address = address;
+		this.city = city;
+		this.telephone = telephone;
+		this.pets = pets;
+	}
+
 	@Column(name = "address")
 	private String address;
 	
--- pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Person.java
+++ pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Person.java
@@ -3,13 +3,24 @@ package guru.springframework.petclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
+	
+
+	public Person(Long id, String firstName, String lastName) {
+		super(id);
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 
 	@Column(name = "first_name")
 	private String firstName;
